5e8a263d3690ea97113094a553e48003f9ae1d9a
ntujvang <ntuj.vang@holbertonschool.com>
2017-01-04 04:44:52 +0000

    change to use {:d}

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,11 +5,10 @@ digit = number % 10
 if number < 0:
     digit = -digit
 if digit > 5:
-    print("Last digit of", number, "is", digit,
-          "and is less than 6 and not 0")
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+          .format(number, digit))
 elif digit == 0:
-    print("Last digit of", number, "is", digit,
-          "and is 0")
+    print("Last digit of {:d} is {:d} and is 0".format(number, digit))
 elif digit < 6:
-    print("Last digit of", number, "is", digit,
-          "and is less than 6 and not 0")
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+          .format(number, digit))
