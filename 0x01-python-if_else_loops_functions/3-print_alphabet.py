9b6686a9311255f7ad9c7753b83c0ae93c3addca
ntujvang <ntuj.vang@holbertonschool.com>
2017-01-04 05:04:27 +0000

    going back to original

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if (i != ord('q') and i != ord('e')):
-        print("{}".format(chr(i)), end="")
+for i in "abcdfghijklmnoprstuvwxyz":
+    print("{}".format(i), end="")
