dbcd42c5b188fa8d24406e6ffbed8ef428d80669
ntujvang <ntuj.vang@holbertonschool.com>
2017-01-06 03:29:09 +0000

    hopefully working?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
-dir(hidden_4)
+if __name__ == "__main__":
+    size = len(dir(hidden_4))
+    for i in range(0, size):
+        if dir(hidden_4)[i][0:2] != "__":
+            print(dir(hidden_4)[i])
