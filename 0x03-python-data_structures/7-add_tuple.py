b66f6496721458397cfa4440ac87112818b8b637
ntujvang <ntuj.vang@holbertonschool.com>
2017-01-11 05:50:28 +0000

    LoL.. typo?

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,7 +7,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tup_a.append(0)
     if len(tup_b) < 2:
         for i in range(len(tup_b), 2):
-            tup_b.appent(0)
+            tup_b.append(0)
     temp_tup = [tup_a[0] + tup_b[0], tup_a[1] + tup_b[1]]
     new_tup = tuple(temp_tup)
     return new_tup
