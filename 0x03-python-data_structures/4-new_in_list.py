4d2da68331dfcfabeb0ed53ca9e594fe20877377
ntujvang <ntuj.vang@holbertonschool.com>
2017-01-11 05:40:00 +0000

    checking for - again?

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx >= len(my_list):
+    if idx >= len(my_list) or idx < 0:
         return my_list
     new_list = []
     for i in range(0, len(my_list)):
