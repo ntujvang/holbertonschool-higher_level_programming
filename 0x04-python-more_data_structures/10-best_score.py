e8795eeab67bc99084bfa5bd167d327acd6d5baa
ntujvang <ntuj.vang@holbertonschool.com>
2017-01-18 05:35:31 +0000

    ?

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,5 @@
 def best_score(my_dict):
     if my_dict is None:
         return None
-    else:
-        highest = max(my_dict, key=my_dict.get)
-        return highest
+    highest = max(my_dict, key=my_dict.get)
+    return highest
