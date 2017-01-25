3441a43ee691800909e7d74da1696b7fbc6bf128
ntujvang <ntuj.vang@holbertonschool.com>
2017-01-25 06:12:45 +0000

    change private to public in init

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.__width = int(width)
-        self.__height = int(height)
+        self.width = int(width)
+        self.height = int(height)
 
     """ this set or prop/setter is for width
     """
