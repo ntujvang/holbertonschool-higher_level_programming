013a827a510aa79d4bbafb222306ddb8588e70fa
ntujvang <ntuj.vang@holbertonschool.com>
2017-02-06 07:18:34 +0000

    pep8 issues

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
-    with open (filename, mode="w", encoding="UTF8") as file:
+    with open(filename, mode="w", encoding="UTF8") as file:
         return file.write(text)
