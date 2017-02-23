448e7dc76d062f543a786fa1e291ccb48174fde9
ntujvang <ntuj.vang@holbertonschool.com>
2017-02-23 04:28:38 +0000

    Value instead of Values?

--- 0x0C-SQL_introduction/9-full_creation.sql
+++ 0x0C-SQL_introduction/9-full_creation.sql
@@ -2,6 +2,7 @@
 -- id(INT), name(VARCHAR(256)), score(INT)
 -- Also creates 4 new rows
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (`id`, `name`, `score`) VALUES
-("1", "John", "10"), ("2", "Alex", "3"),
-("3", "Bob", "14"), ("4", "Geroge", "8");
+-- Inserting information
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUE
+(1, "John", 10), (2, "Alex", 3),
+(3, "Bob", 14), (4, "Geroge", 8);
