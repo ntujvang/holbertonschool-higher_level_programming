bbcf42f6e0150039dac80dfa21f8d5478e3f0f61
ntujvang <96@holbertonschool.com>
2017-03-02 23:58:31 +0000

    rearranged and typo fixing

--- 0x0D-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0D-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Script that lists all shows contained in hbtn_0d_tvshows
 -- In tv_shows.title - tv_shows_genres.genre_id format
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY title, genre_id ASC;
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
