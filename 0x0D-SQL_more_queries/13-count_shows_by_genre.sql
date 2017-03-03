f660ab034dc452bf71ba18a3e8f3042e9598024a
ntujvang <96@holbertonschool.com>
2017-03-03 00:02:22 +0000

    typo

--- 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,6 @@
 -- of shows linked to each
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
 FROM tv_show_genres
-JOIN tv_genres ON tv_genres.id = tv_shows_genres.genre_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY number_shows DESC;
