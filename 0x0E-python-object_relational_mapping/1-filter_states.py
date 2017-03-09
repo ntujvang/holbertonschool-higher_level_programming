5c6f4a86dafdfb3e485694a85881f0308aca770b
ntujvang <96@holbertonschool.com>
2017-03-09 05:09:35 +0000

    added an additional if statement

--- 0x0E-python-object_relational_mapping/1-filter_states.py
+++ 0x0E-python-object_relational_mapping/1-filter_states.py
@@ -3,11 +3,10 @@
 Script uses MySQLdb to access database argv[3] and prints out anything
 starting with "N"
 '''
-import sys
-import MySQLdb
-
-
 if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
     db = MySQLdb.connect(host="localhost",
                          user=sys.argv[1],
                          password=sys.argv[2],
@@ -16,6 +15,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     for row in cur.fetchall():
-        print(row)
+        if row[1][0] == 'N':
+            print(row)
     cur.close()
     db.close()
