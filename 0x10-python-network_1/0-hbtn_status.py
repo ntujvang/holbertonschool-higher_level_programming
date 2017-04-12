8a9199d53c215b0f8d2f31ccedd5a91f52f805f7
ntujvang <96@holbertonschool.com>
2017-04-12 03:58:21 +0000

    removed url variable

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -6,8 +6,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
