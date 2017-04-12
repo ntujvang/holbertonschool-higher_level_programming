b0963d41af82445178eca68a0cc86a424d436c92
ntujvang <96@holbertonschool.com>
2017-04-12 18:55:51 +0000

    maybe with try/except

--- 0x10-python-network_1/10-my_github.py
+++ 0x10-python-network_1/10-my_github.py
@@ -9,6 +9,9 @@ import sys
 if __name__ == "__main__":
     user = sys.argv[1]
     pwd = sys.argv[2]
-    url = "https://api.github.com/users"
-    r = requests.get(url, auth=(user, pwd))
-    print(r.json().get('id'))
+    url = "https://api.github.com/user"
+    try:
+        r = requests.get(url, auth=(user, pwd))
+        print(r.json()["id"])
+    except:
+        print("None")
