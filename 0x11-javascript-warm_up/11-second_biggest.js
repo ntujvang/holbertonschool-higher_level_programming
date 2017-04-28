934a07c3935dd19be588f0b27e00deb794cdf769
ntujvang <96@holbertonschool.com>
2017-04-28 02:30:47 +0000

    maybe?

--- 0x11-javascript-warm_up/11-second_biggest.js
+++ 0x11-javascript-warm_up/11-second_biggest.js
@@ -6,15 +6,16 @@ let big = null;
 let bigger = null;
 if (length <= 3) {
   console.log('0');
-}
-for (let i = 0; (i + 2) < length; i++) {
-  myArray[i] = (Number(process.argv[(i + 2)]));
-}
-myArray.sort();
-for (let i = 0; i < myArray.length; i++) {
-  bigger = myArray[i];
-  if (bigger < myArray[i + 1]) {
-    big = bigger;
+} else {
+  for (let i = 0; (i + 2) < length; i++) {
+    myArray[i] = (Number(process.argv[(i + 2)]));
   }
+  myArray.sort();
+  for (let i = 0; i < myArray.length; i++) {
+    bigger = myArray[i];
+    if (bigger < myArray[i + 1]) {
+      big = bigger;
+    }
+  }
+  console.log(big);
 }
-console.log(big);
