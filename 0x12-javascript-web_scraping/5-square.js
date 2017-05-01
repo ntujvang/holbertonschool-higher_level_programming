6e16f734684fd1dca1132148f36e4d79a92b52d3
ntujvang <96@holbertonschool.com>
2017-05-01 03:54:26 +0000

    removed .js

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // class that defines square and inherits rectangle_v4
-const Rectangle = require('./4-rectangle.js').Rectangle;
+const Rectangle = require('./4-rectangle').Rectangle;
 function Square (size) {
   Rectangle.call(this, size, size);
 }
