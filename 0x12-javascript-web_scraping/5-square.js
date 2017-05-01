70a0ca7bf8704565b11832d43f0688de632ef873
ntujvang <96@holbertonschool.com>
2017-05-01 03:57:45 +0000

    reversion

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 // class that defines square and inherits rectangle_v4
-const Rectangle = require('./4-rectangle').Rectangle;
-function Square (size) {
+const Rectangle = require('./4-rectangle.js').Rectangle;
+module.exports.Square = function Square (size) {
   Rectangle.call(this, size, size);
 }
-exports.Square = Square;
