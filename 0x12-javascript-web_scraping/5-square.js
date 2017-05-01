aada3134f1e0b569e97923b1901d799b26cf533a
ntujvang <96@holbertonschool.com>
2017-05-01 03:53:24 +0000

    moved exports

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 // class that defines square and inherits rectangle_v4
 const Rectangle = require('./4-rectangle.js').Rectangle;
-module.exports.Square = function Square (size) {
+function Square (size) {
   Rectangle.call(this, size, size);
-};
+}
+exports.Square = Square;
